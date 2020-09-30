0dc3903d2a44d883f72f9894211763b8bf0ec140
Deepak Singh <malwareperseus@gmail.com>
2020-09-30 14:56:18 +0530

    Fixed Error

--- pretzels/models.py
+++ pretzels/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Pretzels(models.Model)
+class Pretzels(models.Model):
      # theres alwaus an ID tag, on first pos, with primay key set to true
     content = models.TextField(blank=True, null=True)
     images = models.FileField(upload_to='images/', blank=True, null= True)

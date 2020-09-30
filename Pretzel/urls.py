ce8669d51a5958eb18d8f10093191acdf803f662
Deepak Singh <malwareperseus@gmail.com>
2020-09-30 16:59:14 +0530

    Added path to urls.py | Tried Dynamic URL

--- Pretzel/urls.py
+++ Pretzel/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
-from pretzels.views import home_view
+from pretzels.views import home_view, tweet_detail_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('', home_view)
+    path('', home_view),
+    path('tweets/<int:tweet_id>', tweet_detail_view),
 ]
--- pretzels/views.py
+++ pretzels/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Home View</h1>")
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    return HttpResponse(f'<h1>Tweet {tweet_id} </h1>')

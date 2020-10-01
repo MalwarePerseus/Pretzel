31a9de72be993ae7fa7c00982dc448881f80f80a
Deepak Singh <malwareperseus@gmail.com>
2020-10-01 15:15:58 +0530

    Minor Fix | Put 'pretzel'

--- Pretzel/urls.py
+++ Pretzel/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.urls import path
 from django.contrib import admin
-from pretzels.views import home_view, tweet_detail_view
+from pretzels.views import home_view, pretzel_detail_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('', home_view),
-    path('tweets/<int:tweet_id>', tweet_detail_view),
+    path('pretzels/<int:pretzel_id>', pretzel_detail_view),
 ]

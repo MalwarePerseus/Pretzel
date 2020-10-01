8ccadc070b924367dc26afec6c47a90731fadc75
Deepak Singh <malwareperseus@gmail.com>
2020-10-01 15:45:10 +0530

    Dynamic Views Now Send back Json response | REST API EndPoint

--- pretzels/views.py
+++ pretzels/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponse, Http404, JsonResponse
 
 from .models import Pretzels
 
@@ -9,8 +8,18 @@ def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Home View</h1>")
 
 def pretzel_detail_view(request, pretzel_id, *args, **kwargs):
+
+    data = {
+        "id" : pretzel_id,
+    }
+    
+    status = 200
+
     try:
         obj = Pretzels.objects.get(id = pretzel_id)
+        data['content'] = obj.content
     except:
-        raise Http404
-    return HttpResponse(f'<h1>Pretzel {pretzel_id} - {obj.content} </h1>')
+        data['message'] = "Not Found"
+        status = 404
+
+    return JsonResponse(data, status = status)

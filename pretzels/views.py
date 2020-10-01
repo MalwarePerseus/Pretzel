eea11662082e6eadc62241ce88b7fa5eed1ef8c7
Deepak Singh <malwareperseus@gmail.com>
2020-10-01 15:16:47 +0530

    Added Dynamic pretzel Content fetching Form the DB

--- pretzels/views.py
+++ pretzels/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Pretzels
+
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Home View</h1>")
 
-def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    return HttpResponse(f'<h1>Tweet {tweet_id} </h1>')
+def pretzel_detail_view(request, pretzel_id, *args, **kwargs):
+    try:
+        obj = Pretzels.objects.get(id = pretzel_id)
+    except:
+        raise Http404
+    return HttpResponse(f'<h1>Pretzel {pretzel_id} - {obj.content} </h1>')
